5e8225b85cb1f712cc3d6083cc7a31a5c1d7103d
Vanessa Otto <boehner.vanessa@gmail.com>
2024-05-06 10:53:10 +0200

    Init Layout

---
+++ components/FooterSocialLink.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    label: string;
+    href: string;
+    d?: string;
+    viewBox?: string;
+  }>(),
+  {
+    viewBox: "0 0 24 24",
+  }
+);
+</script>
+
+<template>
+  <a
+    :href
+    target="_blank"
+    ref="noopener noreferrer nofollow"
+    class="text-gray-500 hover:text-gray-400"
+  >
+    <span class="sr-only"> {{ label }} </span>
+    <slot>
+      <svg class="h-6 w-6" fill="currentColor" :viewBox aria-hidden="true">
+        <path :d />
+      </svg>
+    </slot>
+  </a>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import AppFooter from "../components/AppFooter.vue";
+</script>
+
+<template>
+  <div class="min-h-100 flex space-between">
+    <main>
+      <slot />
+    </main>
+    <AppFooter />
+  </div>
+</template>
